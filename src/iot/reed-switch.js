f8c585aca5140a5a9d56b101d8d3d313c497d616
Kien Pham <kien@kienpham.com>
2019-09-08 16:37:28 -0700

    z

--- src/iot/reed-switch.js
+++ src/iot/reed-switch.js
@@ -3,11 +3,17 @@ const { Gpio } = require('onoff');
 const reedSwitchPin = 23;
 
 reed = new Gpio(reedSwitchPin, 'in', 'both');
+reed2 = new Gpio(24, 'in', 'both');
 
 reed.watch((err, value) => {
-  console.log('value is ', value);
+  console.log('reed1 value is ', value);
+});
+
+reed2.watch((err, value) => {
+  console.log('reed2 value is ', value);
 });
 
 process.on('SIGINT', () => {
   reed.unexport();
+  reed2.unexport();
 });
