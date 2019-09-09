3e2bf6975d542049c213816f7b4feaa9c8f5b7ef
Kien Pham <kien@kienpham.com>
2019-09-08 20:38:10 -0700

    z

--- src/iot/reed-switch.js
+++ src/iot/reed-switch.js
@@ -1,19 +1,27 @@
-const { Gpio } = require('onoff');
+const gpio = require("gpio");
 
-const reedSwitchPin = 23;
+// Calling export with a pin number will export that header and return a gpio header instance
+const reed = gpio.export(21, {
+  // When you export a pin, the default direction is out. This allows you to set
+  // the pin value to either LOW or HIGH (3.3V) from your program.
+  direction: gpio.DIRECTION.OUT,
 
-reed = new Gpio(reedSwitchPin, 'in', 'both');
-reed2 = new Gpio(24, 'in', 'both');
+  // set the time interval (ms) between each read when watching for value changes
+  // note: this is default to 100, setting value too low will cause high CPU usage
+  interval: 500,
 
-reed.watch((err, value) => {
-  console.log('reed1 value is ', value);
+  // Due to the asynchronous nature of exporting a header, you may not be able to
+  // read or write to the header right away. Place your logic in this ready
+  // function to guarantee everything will get fired properly
+  ready: function() {
+  }
 });
 
-reed2.watch((err, value) => {
-  console.log('reed2 value is ', value);
-});
+reed.on('change', (val) => {
+  console.log('new val', val);
+})
 
-process.on('SIGINT', () => {
-  reed.unexport();
-  reed2.unexport();
-});
+
+intervalTimer = setInterval(() => {
+  console.log('watching ...');
+}, 1000);
