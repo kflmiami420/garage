a06ca7267a1d4814d79660f54e228e1cf9d058f7
Kien Pham <kien@kienpham.com>
2019-09-17 21:15:54 -0700

    z

--- src/iot/switch.py
+++ src/iot/switch.py
@@ -1,15 +1,49 @@
 import RPi.GPIO as GPIO
 import time
+import os
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTShadowClient
+from dotenv import load_dotenv
+
+load_dotenv()
+
+switchChannel = os.environ['SWITCH_CHANNEL']
+shadowName = os.environ['IOT_CLIENT_ID']
+iotEndpoint = os.environ['IOT_ENDPOINT']
 
 GPIO.setmode(GPIO.BCM)
-pin = 21
-currentState = GPIO.LOW
+currentState = ''
+GPIO.setup(switchChannel, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+ROOT_CA = "certs/AmazonRootCA1.pem"
+PRIVATE_KEY = "private.pem.key"
+CERT_FILE = "certificate.pem.crt"
+SHADOW_HANDLER = "garage"
+
+# Automatically called whenever the shadow is updated.
+def myShadowUpdateCallback(payload, responseStatus, token):
+  print('UPDATE: $aws/things/' + SHADOW_HANDLER +
+    '/shadow/update/#')
+  print("payload = " + payload)
+  print("responseStatus = " + responseStatus)
+  print("token = " + token)
+
+shadowClient = AWSIoTMQTTShadowClient(shadowName)
+shadowClient.configureEndpoint(iotEndpoint, 8883)
+shadowClient.configureCredentials(ROOT_CA, PRIVATE_KEY, CERT_FILE)
+shadowClient.configureConnectDisconnectTimeout(10)
+shadowClient.configureMQTTOperationTimeout(5)
+shadowClient.connect()
+
+deviceShadow = shadowClient.createShadowHandlerWithName(SHADOW_HANDLER, True)
 
 while True:
     val = GPIO.input(pin)
     if currentState != val:
         currentState = val
-        print('val ', val)
+        if currentState == GPIO.HIGH:
+            print('garage open')
+        else:
+            print('garage close')
+        # deviceShadow.shadowUpdate('{"state":{"reported":{"door_X":"open"}}}', myShadowUpdateCallback, 5)
+
     time.sleep(0.5)
